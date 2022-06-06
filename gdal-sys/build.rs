1cbe40dea5d8890f8f9e0224bcfbe40041873203
SparkyPotato <noob.sparkypotato@gmail.com>
2022-06-06 23:01:11 +0530

    automatically download gdal library

--- gdal-sys/build.rs
+++ gdal-sys/build.rs
@@ -1,10 +1,35 @@
+use std::io::{Cursor, Read};
 use std::path::Path;
+use zip::ZipArchive;
 
 fn main() {
-    println!(
-        "cargo:rustc-link-search=native={}",
-        std::fs::canonicalize(Path::new("vendor"))
-            .unwrap()
-            .display()
-    );
+    if cfg!(windows) {
+        let gdal_path = Path::new("vendor/gdal.lib");
+        if !gdal_path.exists() {
+            let data = reqwest::blocking::get(
+                "https://drive.google.com/u/1/uc?id=1OF0OAZPHd7pGCgDhTHQ0-QAt7GI4xqT1&export=download&confirm=t",
+            )
+                .unwrap()
+                .bytes()
+                .unwrap();
+
+            let mut archive = ZipArchive::new(Cursor::new(&data[..])).unwrap();
+            let mut data = Vec::new();
+            archive.by_index(0).unwrap().read_to_end(&mut data).unwrap();
+            std::fs::write("vendor/gdal.lib", data).unwrap();
+
+            println!(
+                "cargo:rustc-link-search=native={}",
+                std::fs::canonicalize(Path::new("vendor"))
+                    .unwrap()
+                    .display()
+            );
+        }
+    } else {
+        println!("cargo:rerun-if-env-changed=GDAL_PATH");
+        let path = std::env::var("GDAL_PATH")
+            .expect("Expected environment variable GDAL_PATH to point to a static GDAL library");
+
+        println!("cargo:rustc-link-search=native={}", path);
+    }
 }
