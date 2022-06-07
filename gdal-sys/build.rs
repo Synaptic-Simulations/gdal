942e28c015492b6a4a3d45fa500cf5ea20401cb9
SparkyPotato <noob.sparkypotato@gmail.com>
2022-06-07 17:36:08 +0530

    fix autodownload (again)

--- gdal-sys/build.rs
+++ gdal-sys/build.rs
@@ -4,7 +4,9 @@ use zip::ZipArchive;
 
 fn main() {
     if cfg!(windows) && std::env::var_os("GDAL_NO_DOWNLOAD").is_none() {
-        let gdal_path = Path::new("vendor/gdal.lib");
+		let out = std::env::var_os("OUT_DIR").unwrap();
+        let out_path = Path::new(&out);
+        let gdal_path = out_path.join("gdal.lib");
         if !gdal_path.exists() {
             let data = reqwest::blocking::get(
                 "https://drive.google.com/u/1/uc?id=1OF0OAZPHd7pGCgDhTHQ0-QAt7GI4xqT1&export=download&confirm=t",
@@ -16,14 +18,11 @@ fn main() {
             let mut archive = ZipArchive::new(Cursor::new(&data[..])).unwrap();
             let mut data = Vec::new();
             archive.by_index(0).unwrap().read_to_end(&mut data).unwrap();
-            std::fs::create_dir_all("vendor").unwrap();
-            std::fs::write(gdal_path, data).unwrap();
+            std::fs::write(&gdal_path, data).unwrap();
 
             println!(
                 "cargo:rustc-link-search=native={}",
-                std::fs::canonicalize(Path::new("vendor"))
-                    .unwrap()
-                    .display()
+                out_path.display()
             );
         }
     } else {
